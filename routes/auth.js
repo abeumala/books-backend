165c7a463d8a2f9f42678068b3795be11e75470b
Arnau Beumala Garcia <arnaubeumalagarcia@MacBook-Air-de-Arnau.local>
2019-05-25 23:09:12 +0200

    auth router fixed

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,7 @@ const authAPI 		= require('../api/authAPI');
 
 module.exports = (app, router) => {
 	
-	router.post('/register', {
-		console.log("in backend route")
-		authAPI.register
-		});
+	router.post('/register', authAPI.register);
 
 	router.post('/authenticate', authAPI.authenticate);
 
