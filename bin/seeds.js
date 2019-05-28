f2f75335b6e22b9e1c24f8fd8d15de76241a8bd6
Arnau Beumala Garcia <arnaubeumalagarcia@MacBook-Air-de-Arnau.local>
2019-05-28 15:33:28 +0200

    update seed

---
+++ bin/seeds.js
@@ -0,0 +1,65 @@
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+    .then(x => console.log('MongoDB connected'))
+    .catch(err => {
+      console.error('Error connecting to mongo', err);
+  });
+
+const Book = require('./../models/book');
+const books =[
+  {
+    title: "Beyond good and evil",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    author: "F. Nietzsche",
+    coverUrl: "book1Cover.jpg"
+  },
+
+  {
+    title: "The Republic",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    author: "Plato",
+    coverUrl: "book2Cover.jpg"
+  },
+
+  {
+    title: "Being and Time",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    author: "M. Heidegger",
+    coverUrl: "book3Cover.jpg"
+  },
+
+  {
+    title: "Discipline and Punish",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    author: "Michael Foucault",
+    coverUrl: "book4Cover.jpg"
+  },
+
+  {
+    title: "Critique of Pure Reason",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    author: "Immanuel Kant",
+    coverUrl: "book5Cover.jpg"
+  }
+]
+
+const getBooks = () => {
+
+    // console.log(result);
+    books.forEach((book) => {
+      const { title, description, author, coverUrl } = book;
+
+          const newBook = new Book({ title, description, author, coverUrl  });
+
+        newBook.save()
+        .then((result) => console.log('Created book', result.title))
+        .catch(error => console.log('heep' + error));
+    return;
+    });
+}
+
+getBooks();
